19420ee4f6e0c68c38b3d96f95f105b608caa78e
Lernej <noahklern@gmail.com>
2025-09-27 09:20:36 -0400

    Added Authentication Page

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// firebaseConfig.ts
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmEyGrH_W5xVXPlNVukUGanRZPo8gJniY",
+  authDomain: "shellhacks-2025-studyfinder.firebaseapp.com",
+  projectId: "shellhacks-2025-studyfinder",
+  storageBucket: "shellhacks-2025-studyfinder.appspot.com",
+  messagingSenderId: "521476233651",
+  appId: "1:521476233651:web:c4c7ecf5659e5508ef13c6",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
